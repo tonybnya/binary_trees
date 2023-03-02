18132cf9e4887da1faa9d78a6d204469079d964f
Tony Blondeau NYA <nya.tony2010@gmail.com>
2023-03-02 13:39:24 -0800

    update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,18 +10,18 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-        binary_tree_t *node;
+	binary_tree_t *node;
 
-        node = binary_tree_node(parent, value);
-        if (node == NULL)
-                return (NULL);
+	node = binary_tree_node(parent, value);
+	if (node == NULL)
+		return (NULL);
 
-        if (parent->left != NULL)
-        {
-                parent->left->parent = node;
-                node->left = parent->left;
-        }
-        parent->left = node;
+	if (parent->left != NULL)
+	{
+		parent->left->parent = node;
+		node->left = parent->left;
+	}
+	parent->left = node;
 
-        return (node);
+	return (node);
 }
