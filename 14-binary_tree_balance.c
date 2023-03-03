03a02d68aa8c1ba8221196a8dc1b83275e95e985
Tony Blondeau NYA <nya.tony2010@gmail.com>
2023-03-03 14:09:37 -0800

    Update: Task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -31,8 +31,8 @@ size_t bt_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        return (bt_height(tree->left) - bt_height(tree->right));
+	return (bt_height(tree->left) - bt_height(tree->right));
 }
