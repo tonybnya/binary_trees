d0ed6249b940efa6835652f26390ecb406999a8f
Tony Blondeau NYA <nya.tony2010@gmail.com>
2023-03-03 05:20:30 -0800

    Update: Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t lh = 0, rh = 0;
+	size_t lh = 0, rh = 0;
 
 	if (tree == NULL)
 		return (0);
 
-        lh = tree->left ? binary_tree_height(tree->left) + 1 : 0;
-        rh = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+	lh = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+	rh = tree->right ? binary_tree_height(tree->right) + 1 : 0;
 
-        return (max(lh, rh));
+	return (max(lh, rh));
 }
