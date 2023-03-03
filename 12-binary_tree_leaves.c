b6344c38cd5b6ced800b672d5872130f57a8895c
Tony Blondeau NYA <nya.tony2010@gmail.com>
2023-03-03 13:26:20 -0800

    Update: Task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,15 +10,15 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-        size_t leaves;
+	size_t leaves;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        if (tree->left == NULL && tree->right == NULL)
-                return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-        leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
 
-        return (leaves);
+	return (leaves);
 }
