201bf414673a4640a55709db223c5b5517ce3bbe
Tony Blondeau NYA <nya.tony2010@gmail.com>
2023-03-03 13:40:16 -0800

    Update: Task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,16 +9,16 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-        size_t nodes;
+	size_t nodes;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        if (tree->left || tree->right)
-        {
-                nodes = 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
-                return (nodes);
-        }
+	if (tree->left || tree->right)
+	{
+		nodes = 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+		return (nodes);
+	}
 
-        return (0);
+	return (0);
 }
